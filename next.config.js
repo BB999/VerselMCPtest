85eeffa0e607dcd03d8e46d0935b92337848a44a
noranekob <minimal_ai_lifestyle@icloud.com>
2025-09-08 16:03:58 +0900

    全部消した

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: []
-}
-
-module.exports = nextConfig
--- src/app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  return Response.json({ message: 'Hello from Vercel!' })
-}
-
-export async function POST() {
-  return Response.json({ message: 'POST Hello from Vercel!' })
-}
